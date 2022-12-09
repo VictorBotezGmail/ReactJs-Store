58a1f1f3e0c9eb695ad87e1e7d9996ed795e8c2e
Victor Botez <victor.botez00@gmail.com>
2022-12-09 16:03:24 +0200

    End of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -25,7 +25,7 @@ namespace API
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
 
             services.AddDbContext<StoreContext>(opt => 
@@ -34,6 +34,8 @@ namespace API
             }
 
             );
+
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,12 +45,16 @@ namespace API
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
 
             //app.UseHttpsRedirection();
 
             app.UseRouting();
+           app.UseCors(o =>
+            {
+                o.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("http://localhost:3000");
+            });
 
             app.UseAuthorization();
 
